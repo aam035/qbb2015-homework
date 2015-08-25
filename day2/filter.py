5e11668951eed7e80c390646265e206fe906b24a
Aurelia Mapps <amapps1@jhu.edu>
2015-08-25 11:24:37 -0400

    Filter lines 10 to 15

--- day2/filter.py
+++ day2/filter.py
@@ -4,10 +4,18 @@ filename="/Users/cmdb/qbb2015/stringtie/SRR072893/t_data.ctab"
 
 f = open( filename )
 
+
 #files are itterables that can be used in list, commas surpress new lines after file is printed
-for line in f:
-    fields = line.split()
-    if "tRNA" in fields[9]:
+for line_count, line in enumerate(f): #enumerate adds the increments and does not need line_count +=1
+    if line_count <=10:#limits the lines to 10 to 15
+        pass #does nothing
+    elif line_count <= 15:
         print line,
+    else:
+        break #to stop looking through lines
+    
+    
+        
+    
 
 
