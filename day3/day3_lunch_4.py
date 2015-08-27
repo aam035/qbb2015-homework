3f1c3abb4f5e780b25bd51049d1e1eeca14aac1f
Aurelia Mapps <amapps1@jhu.edu>
2015-08-27 06:43:57 -0400

    Made changes to the fourth one

--- day3/day3_lunch_4.py
+++ day3/day3_lunch_4.py
@@ -4,6 +4,11 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
 
+#MA Plots are used to analyze gene expression from microarray data
+    #if not logged, the data looks very linear; log allows the user to observe data points that aren't on the linear path
+    #by using an MA Plot, we turn the data at a 45 degree angle and compare the average intensity of gene expression to the     mean of gene expression
+ 
+# we need two files to compare and set to zero
 df = pd.read_table("~/qbb2015/stringtie/SRR072893/t_data.ctab")
 df2 = df["FPKM"] > 0 #can't have the [df[df[""]]] here???
 
@@ -20,11 +25,11 @@ mf3 = mf[b1]["FPKM"]
 
 #print mf3 check work again
 
-df4 = np.log(df3)
-mf4 = np.log(mf3)
+df4 = np.log2(df3) #this takes the log base 2 of the data
+mf4 = np.log2(mf3)
 
-m = df4 - mf4
-a = 0.5 * (df4 + mf4)
+m = df4 - mf4 #mean of intensity
+a = 0.5 * (df4 + mf4) #average intensity
 
 plt.figure()
 plt.scatter(a, m)
