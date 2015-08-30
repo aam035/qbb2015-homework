0b23785da2e487c480f382d6942715367e9ff4c6
Aurelia Mapps <amapps1@jhu.edu>
2015-08-29 21:32:39 -0400

    Readding the number 2 to print out file names

--- day3/day3_lunch2.py
+++ day3/day3_lunch2.py
@@ -9,9 +9,9 @@ df = pd.read_table("~/qbb2015/stringtie/SRR072893/t_data.ctab")
 df2 = df[df["FPKM"] > 0] #to get rid of zeros out of the file
 
 
-plt.figure()
-plt.hist(df2["FPKM"].values)
-plt.savefig("histogram.png")
+#plt.figure()
+#plt.hist(df2["FPKM"].values)
+#plt.savefig("histogram.png")
 
 plt.figure()
 plt.hist(np.log(df2["FPKM"].values))
